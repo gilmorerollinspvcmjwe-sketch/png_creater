4cea8b200db16b76ad839f5040c04127856c8f9c
beiaojue <gilmorerollinspvcmjwe@gmail.com>
2026-04-29 09:51:12 +0800

    Delete heya-studio directory

--- heya-studio/frontend/src/components/agent/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as AIChatPanel } from './AIChatPanel'
-export { default as GuidedWizard } from './GuidedWizard'
-export { default as MiniPreview } from './MiniPreview'
--- heya-studio/frontend/src/hooks/index.ts
+++
@@ -1,2 +0,0 @@
-export { useKeyboardShortcuts } from './useKeyboardShortcuts'
-export { useContainerWidth } from './useContainerWidth'
